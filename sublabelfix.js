751ffc4c44980934b10a93a0045c0dde00bae5a4
Maxim Shchigrinets <30348120+Jeshix@users.noreply.github.com>
2017-10-06 14:25:25 +0300

    Update sublabelfix.js

--- sublabelfix.js
+++ sublabelfix.js
@@ -1,15 +1,1 @@
-jq_144(document).ready(function($) {
-	$('input[type=radio]').each(function() {
-		if($(this).parent().hasClass('form-list-item') && $(this).attr('name').substr(0, 7) == 'widget_') {
-			$(this).parent().contents().filter(function () {
-				return this.nodeType === 3;
-			}).wrap('<sublabel></sublabel>');
-		}
-	});
-	$('sublabel').each(function() {
-		$(this).css('cursor', 'default');
-	});
-	$('sublabel').on('click', function() {
-		$(this).prev().click();
-	});
-});
+jq_144(document).ready(function(t){t("input[type=radio]").each(function(){t(this).parent().hasClass("form-list-item")&&"widget_"==t(this).attr("name").substr(0,7)&&t(this).parent().contents().filter(function(){return 3===this.nodeType}).wrap("<sublabel></sublabel>")}),t("sublabel").each(function(){t(this).css("cursor","default")}),t("sublabel").on("click",function(){t(this).prev().click()})});
